1bbeb22e711613525c926a3e9f32d3af6c8164d7
gplatono <gr.plat@gmail.com>
2022-09-11 22:15:21 -0700

    Update template and added a couple of CF solutions

--- codeforces/112B.cpp
+++ codeforces/112B.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main(int argc, char** argv) {
     int n, x, y;
     cin >> n >> x >> y;
-    if (abs(n/2 - x) == 1 && abs(n/2 - y) == 1)
+    n /= 2;
+    if (n <= x && x <= n+1 && n <= y && y <= n+1)
         cout << "NO";
     else
         cout << "YES";
--- codeforces/281A.cpp
+++ codeforces/281A.cpp
@@ -5,5 +5,10 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+    char s[1001];
+    cin >> s;
+    if (s[0] > 90)
+        s[0] -= 32;
+    cout << s << endl;
     return 0;
 }
